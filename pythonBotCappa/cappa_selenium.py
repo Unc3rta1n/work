53b76b297331b6e669d61551d6148a44cb5ca3f1
Nikita Yamatin <yamatin_nikita@cybersec74.ru>
2024-07-17 15:04:13 +0500

    переписал заполнение полей на словарь+цикл по нему + вытаскиваю ошибку(пароль<5 символов, почта\логин уже занята и тд) при регистрации

--- pythonBotCappa/cappa_selenium.py
+++ pythonBotCappa/cappa_selenium.py
@@ -19,7 +19,7 @@ class CappaRegAuth:
         self.options = EdgeOptions()
         self.options.add_argument("start-maximized")
         self.options.add_argument("--disable-blink-features=AutomationControlled")
-        #self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--no-sandbox")
         self.options.add_argument("--disable-extensions")
         self.options.add_argument('--disable-application-cache')
         self.options.add_argument('--disable-gpu')
@@ -45,41 +45,32 @@ class CappaRegAuth:
                 driver.save_screenshot('step3_after_second_click.png')
                 # Перешли на https://cappa.csu.ru/auth/signup/
 
-                # Теперь заполняем 6 полей на сайте полями из класса
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_username"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.username)
-                driver.save_screenshot('step4_after_username.png')
+                fields = {
+                    '//*[@id="id_username"]': self.username,
+                    '//*[@id="id_email"]': self.email,
+                    '//*[@id="id_first_name"]': self.first_name,
+                    '//*[@id="id_last_name"]': self.last_name,
+                    '//*[@id="id_password1"]': self.password,
+                    '//*[@id="id_password2"]': self.password,
+                }
 
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_email"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.email)
-                driver.save_screenshot('step5_after_email.png')
-
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_first_name"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.first_name)
-                driver.save_screenshot('step6_after_first_name.png')
-
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_last_name"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.last_name)
-                driver.save_screenshot('step7_after_last_name.png')
-
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_password1"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.password)
-                driver.save_screenshot('step8_after_password1.png')
-
-                cappa_text_field = driver.find_element(By.XPATH, '//*[@id="id_password2"]')
-                cappa_text_field.click()
-                cappa_text_field.send_keys(self.password)
-                driver.save_screenshot('step9_after_password2.png')
+                for xpath, value in fields.items():
+                    cappa_text_field = driver.find_element(By.XPATH, xpath)
+                    cappa_text_field.click()
+                    cappa_text_field.send_keys(value)
                 # клик на кнопку регистрации
-                cappa_button = driver.find_element(By.XPATH, '/html/body/div/main/div/div[2]/div/form/input[3]')
+                cappa_button = driver.find_element(By.XPATH,
+                                                   '/html/body/div/main/div/div[2]/div/form/input[3]')
                 cappa_button.click()
                 time.sleep(2)  # wait for the next page to load
                 driver.save_screenshot('step10_after_registration_click.png')
+                if driver.current_url == 'https://cappa.csu.ru/':
+                    print(f'Пользователь с логином {self.username} успешно зарегистрирован')
+                else:
+                    print(f'Произошла какая-то ошибка')
+                    cappa_error = driver.find_element(By.XPATH,
+                                                      '/html/body/div/main/div/div[2]/div/form/small/ul/li')
+                    print(cappa_error.text)
                 # ну и как бы вроде как зарегались но не понятно
 
         except Exception as e:
@@ -90,12 +81,10 @@ class CappaRegAuth:
         try:
             with Edge(options=self.options) as driver:
                 driver.get(self.url)
-                # Дополнительный код для авторизации пользователя
-                pass
+
         except Exception as e:
             print(f"Ошибка при авторизации: {e}")
-            # Дополнительная обработка ошибок
 
 
-Cappa = CappaRegAuth('sakldadklakdlsajkldasjkldasd', '12345', 'dsaddsadsa@ya.ru', 'sdadsadsa', 'daadsdasadsdas')
+Cappa = CappaRegAuth('sakldadkl123akdlsajkldasjkldasd', '123', 'dsaddsa11dsa@ya.ru', 'sdadsadsa', 'daadsdasadsdas')
 Cappa.registrate()
